f1f978cfece651db7abc0d43f85fa454160eb8da
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-11 18:16:41 -0500

    got random pick working. Also that random id's strain name is output to the screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,4 @@
-$( document ).ready(function() {
-  console.log( "Here is our Javascript!" );
+$(document).ready(function () {
   //*** Personal Remedies API ***//
   //*** code by Nate King ***//
   /************
@@ -20,18 +19,72 @@ $( document ).ready(function() {
   **************/
 
 
- prProbIDs = [18, 21, 58, 60, 144, 157, 165, 190, 224, 229];  
-  
- //PR API url
- let prURL = `https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/suggest?subscriptionId=1&problemId=${prProbIDs[0]}&fg2=k2`
+  prProbIDs = [18, 21, 58, 60, 144, 157, 165, 190, 224, 229];
 
- $.ajax({
-   url: prURL,
-   method: "GET",
- }).then(function(herbs){
-   console.log(herbs)
- })
+  //PR API url
+  let prURL = `https://nutridigm-api-dev.azurewebsites.net/api/v1/nutridigm/suggest?subscriptionId=1&problemId=${prProbIDs[0]}&fg2=k2`
 
+  $.ajax({
+    url: prURL,
+    method: "GET",
+  }).then(function (herbs) {
+    //  console.log(herbs)
+  })
+
+
+
+  let effectURL = `https://strainapi.evanbusse.com/WDnZQMY/strains/search/effect/Insomnia`
+
+  $.ajax({
+    url: effectURL,
+    method: "GET",
+  }).then(function (effect) {
+    console.log(effect);
+
+    //Storage array for randomly generated strain ID's
+    let strainRecID = []
+
+    //for loop to pick random strain ID's
+    for (let i = 1; i < 6; i++) {
+      let randoPick = Math.floor(Math.random() * (effect.length - 1));
+      strainRecID.push(effect[randoPick])
+    }
+    console.log(strainRecID)
+    //for loop to create a li element for strainRecId's length and write the name of the strain in each li element. Element id corresponds with strain ID
+
+    for (let i = 0; i < strainRecID.length; i++) {
+      $('<li>').attr('id', strainRecID[i].id).appendTo('#strain-list');
+      // console.log(`#${strainRecID[i].id}`)
+      $(`#${strainRecID[i].id}`).text(strainRecID[i].name);
+    }
+
+    $('li').on('click', function (e) {
+      e.preventDefault();
+      let $strainID = $(this).attr('id');
+
+      let flavorURL = `https://strainapi.evanbusse.com/WDnZQMY/strains/data/flavors/${$strainID}`
+
+      $('li').on('click', function(e){
+        e.preventDefault();
+        let $strainID = $(this).attr('id');
+
+        let flavorURL = `https://strainapi.evanbusse.com/WDnZQMY/strains/data/flavors/${$strainID}`
+        $.ajax({
+          url: flavorURL,
+          method: "GET",
+        }).then(function (effect) {
+
+
+
+        })
+        
+      
+      
+      
+      })//End of li click event listener
+    })
+
+  })
 
 
 });
